e8ed590d34feb002096d9f3e08c060ff7f7bf10b
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2022-12-23 15:46:50 -0500

    Character filter

--- pytubeModule.py
+++ pytubeModule.py
@@ -1,15 +1,24 @@
 from pytube import Playlist
 
-def filterIllegalChars():
-    pass
 
-def downloadPlaylist(url: str, dir: str):
+def filterIllegalChars(title: str) -> str:
+    chars = ['#', '%', '\\', '/', ':', '*', '?', '"', '<', '>', '|']
+    new_title = title
+
+    for char in chars:
+        new_title.replace(char, " ")
+
+    return new_title
+
+
+def downloadPlaylist(url: str, my_dir: str):
     p = Playlist(url)
 
     for vid in p.videos:
         vid.use_oauth = True
         vid.allow_oauth_cache = True
-        vid.streams.get_audio_only().download(output_path=dir, filename=vid.title + ".mp3", skip_existing=True)
+        title = filterIllegalChars(vid.title)
+        vid.streams.get_audio_only().download(output_path=my_dir, filename=title + ".mp3", skip_existing=True)
         print(f"{vid.title} has downloaded!")
 
 
